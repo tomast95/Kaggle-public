db6c4deb4cd3a62612b2988b776971c85f54c4e0
Tomáš Trdla <tomas.trdla@devinn.cz>
2021-12-15 12:30:59 +0100

    Other models + Exploratory analysis with pandas-profiling

--- Titanic/mian.py
+++ Titanic/mian.py
@@ -203,9 +203,54 @@ df_predicted.to_csv("submission.csv", sep=",", index=False)
 
 
 
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.datasets import make_moons, make_circles, make_classification
+from sklearn.neural_network import MLPClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 
+from sklearn.linear_model import LogisticRegression
 
+names = [
+    "Logistic Regression",
+    "Nearest Neighbors",
+    "Linear SVM",
+    "RBF SVM",
+    "Gaussian Process",
+    "Decision Tree",
+    "Random Forest",
+    "Neural Net",
+    "AdaBoost",
+    "Naive Bayes",
+    "QDA",
+]
 
+classifiers = [
+    LogisticRegression(),
+    KNeighborsClassifier(3),
+    SVC(kernel="linear", C=0.025),
+    SVC(gamma=2, C=1),
+    GaussianProcessClassifier(1.0 * RBF(1.0)),
+    DecisionTreeClassifier(max_depth=5),
+    RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),
+    MLPClassifier(alpha=1, max_iter=1000),
+    AdaBoostClassifier(),
+    GaussianNB(),
+    QuadraticDiscriminantAnalysis(),
+]
+
+# iterate over classifiers
+for name, clf in zip(names, classifiers):
+    clf.fit(train_dummy, train['Survived'])
+    score = clf.score(test_dummy, gender_submission['Survived'])
+    print("Clf {0} scored: {1}".format(name, score))
 
 
 
